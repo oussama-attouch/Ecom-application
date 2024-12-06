77d7cad27e77e0e39a7885b8cfdacae7bf5d95b3
OUSSAMA ATTOUCH <73898127+Oussama-Att@users.noreply.github.com>
2024-12-06 21:03:24 +0100

    first commit

---
+++ ecom-app/customer-service/src/main/java/oss/att/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package oss.att.customerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+
+}
