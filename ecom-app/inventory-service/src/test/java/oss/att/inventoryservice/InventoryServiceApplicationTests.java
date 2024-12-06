6aeb846d19eaea089918b4bb9755d930370b0065
OUSSAMA ATTOUCH <73898127+Oussama-Att@users.noreply.github.com>
2024-12-06 21:05:23 +0100

    first commit

---
+++ ecom-app/inventory-service/src/test/java/oss/att/inventoryservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package oss.att.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
